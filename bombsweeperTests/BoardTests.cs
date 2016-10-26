62b8602653250fd73e5c72c309a74dc4102b20bd
durwood <durwood@enlearn.org>
2016-10-26 13:10:54 -0700

    Reorganized solution to include a unit test project; add unit tests around displaying board.

---
+++ bombsweeperTests/BoardTests.cs
@@ -0,0 +1,19 @@
+﻿using bombsweeper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bombsweeperTests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void BoardDisplaysProperly()
+        {
+            var c = Cell.Block;
+            var testObj = new Board(2);
+            string expected = $"{c} {c} \r\n{c} {c} \r\n";
+            var result = testObj.Display();
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ bombsweeperTests/CellTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using bombsweeper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bombsweeperTests
+{
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void NewCellDisplaysProperly()
+        {
+            var testObj = new Cell();
+            Assert.AreEqual(testObj.Display(), $"{Cell.Block}");
+        }
+    }
+}
